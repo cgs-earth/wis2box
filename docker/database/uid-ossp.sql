e3e7294ac4bf84e6526d606943676b9cc1db71ee
Benjamin Webb <bwebb@lincolninst.edu>
2023-11-16 11:19:53 -0500

    Update docker setup

---
+++ docker/database/uid-ossp.sql
@@ -0,0 +1,2 @@
+-- init-uuid-ossp.sql
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- docker/mosquitto/entrypoint.sh
+++ docker/mosquitto/entrypoint.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-echo "mosquitto_passwd -b -c /mosquitto/config/password.txt $WIS2BOX_BROKER_USERNAME $WIS2BOX_BROKER_PASSWORD"
+if [ -f /tmp/wis2box.crt ]; then
+    echo "SSL enabled"
+    echo "setup /mosquitto/certs"
+    mkdir -p /mosquitto/certs
+    cp /tmp/wis2box.crt /mosquitto/certs
+    cp /tmp/wis2box.key /mosquitto/certs
+    chown -R mosquitto:mosquitto /mosquitto/certs
+    cp -f /mosquitto/config/mosquitto-ssl.conf /mosquitto/config/mosquitto.conf
+else
+    echo "SSL disabled"
+fi
 
-mosquitto_passwd -b -c /mosquitto/config/password.txt $WIS2BOX_BROKER_USERNAME $WIS2BOX_BROKER_PASSWORD
+echo "Setting mosquitto authentication"
+if [ ! -e "/mosquitto/config/password.txt" ]; then
+    echo "Adding wis2box users to mosquitto password file"
+    mosquitto_passwd -b -c /mosquitto/config/password.txt $WIS2BOX_BROKER_USERNAME $WIS2BOX_BROKER_PASSWORD
+    mosquitto_passwd -b /mosquitto/config/password.txt everyone everyone
+else
+    echo "Mosquitto password file already exists. Skipping wis2box user addition."
+fi
 
-/usr/sbin/mosquitto -v -c /mosquitto/config/mosquitto.conf
+/usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
